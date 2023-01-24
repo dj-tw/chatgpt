b0ea5557f947a57e5ae4eff07e48b68a07c2dc5e
David Johnston <dajohnst@thoughtworks.com>
2023-01-24 13:51:48 -0500

    add doc strings, refactor, added show_history

--- chatgpt/chat.py
+++ chatgpt/chat.py
@@ -1,18 +1,23 @@
 from api_key import api_key
 import openai
-import uuid
 
-
-# openai.organization = "something"
 openai.api_key = api_key
 
 class Chat:
     def __init__(self, engine="text-davinci-003"):
+        """
+        Instantiate, store the engine and a conversation log
+        :param engine:
+        """
         self.model_engine = engine
-        self.conversation_id = uuid.uuid1().hex
         self.history = "Reply in this fashion, BOT: <ANSWER>"
 
-    def chat(self, prompt):
+    def _chat(self, prompt):
+        """
+        Raw chat, if you want to remember history use __call__
+        :param prompt:
+        :return:
+        """
         completion = openai.Completion.create(
             engine=self.model_engine,
             prompt=prompt,
@@ -25,27 +30,21 @@ class Chat:
         response = completion.choices[0].text
         return response
 
-    def __call__(self, prompt):
+    def clear_conversation(self):
+        """
+        Forget what has been discussed and start over fresh
+        :return: None
+        """
+        self.history = ""
 
-        print('----- History  ----')
-        print(self.history)
-        print('------------')
-
+    def __call__(self, prompt, show_history=False):
         full_prompt = f"conversation_history=({self.history}), USER: {prompt}"
-        print('----- Full prompt-----')
-        print(full_prompt)
-        print('------------------')
 
-        response = self.chat(full_prompt)
+        if show_history:
+            print('----- Full prompt-----')
+            print(full_prompt)
+            print('------------------')
+
+        response = self._chat(full_prompt)
         self.history += f'\nUSER: {prompt}, \n{response}'
         print(response)
-
-def run():
-    c = Chat()
-    while True:
-        message = input()
-        c(message)
-
-
-if __name__ == "__main__":
-    run()
